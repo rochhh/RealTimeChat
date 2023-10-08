e44c2587348c15ab56e64f407650ec5e02ae5cf3
Roch <rochishnu7@gmail.com>
2023-10-08 23:40:36 +0530

    join and leave room logic

--- app.py
+++ app.py
@@ -69,6 +69,42 @@ def room():
     return render_template("room.html")
 
 
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+
+    if not room or not name:
+        return 
+    
+    if room not in rooms:
+        return 
+    
+    join_room(room)
+    send({"name":name , "message":"has entered the room {room} "} , to=room )
+    rooms[room]["members"] += 1
+
+    print(f"{name} joined the room ")
+
+
+@socketio.on("disconnect")
+def disconnect():
+
+    room = session.get("room")
+    name = session.get("name")
+
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]['members'] -= 1
+
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name":name , "message":"has left the room {room}"} , to=room )
+    print(f"{name} joined the room ")
+
+
 if __name__ == "__main__":
     socketio.run(app , debug = True)
 
